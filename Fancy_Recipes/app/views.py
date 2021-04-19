81b1ff9d4eded91068f7719fc12c00e3e52ceba9
Dariusz Lesiecki <65817574+dariuszlesiecki@users.noreply.github.com>
2021-04-19 21:59:58 +0200

    added register and login templates

--- Fancy_Recipes/app/views.py
+++ Fancy_Recipes/app/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 
 
 def index(request):
     template = loader.get_template('index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def register(request):
+    template = loader.get_template('register.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def login(request):
+    template = loader.get_template('login.html')
     context = {}
     return HttpResponse(template.render(context, request))
