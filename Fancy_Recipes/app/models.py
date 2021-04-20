c30cafc6937ea19b17e86028320076378c953c01
Karol Sawicki <karolsawickiaaq@outlook.com>
2021-04-20 18:57:15 +0200

    Established connection with database.

--- Fancy_Recipes/app/models.py
+++ Fancy_Recipes/app/models.py
@@ -1,3 +1,4 @@
+import psycopg2
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
@@ -7,18 +8,18 @@ class Account(models.Model):
     ACCOUNT_TYPES = [("U", 'user'), ("A", 'admin'), ("C", 'cook')]
 
     user = models.OneToOneField(User, on_delete = models.CASCADE)
-    name = models.CharField(50, blank = False)
+    name = models.CharField(max_length = 50, blank = False)
     join_date = models.DateField(auto_now_add = True, blank = True)
-    role = models.CharField(choices = ACCOUNT_TYPES, default = "U")
+    role = models.CharField(max_length = 1, choices = ACCOUNT_TYPES, default = "U")
 
 
 class Ingredient(models.Model):
-    name = models.CharField(50, blank = False)
-    price = models.DecimalField()
+    name = models.CharField(max_length = 50, blank = False)
+    price = models.DecimalField(max_digits = 5, decimal_places = 2)
 
 
 class Recipe(models.Model):
-    name = models.CharField(50, blank = False)
+    name = models.CharField(max_length = 50, blank = False)
     text = models.TextField(blank = False)
     add_date = models.DateField(auto_now_add = True, blank = True)
     author = models.ForeignKey(Account, on_delete = models.CASCADE)
@@ -28,4 +29,3 @@ class Comment(models.Model):
     author = models.ForeignKey(Account, on_delete = models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE)
     text = models.TextField(blank = False)
-
--- Fancy_Recipes/website/settings.py
+++ Fancy_Recipes/website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'yidkdgrz',
+            'USER': 'yidkdgrz',
+            'PASSWORD': 'FxE7YIPpzFEc0q1ejcjT6q7YNWt0IdIO',
+            'HOST': 'rogue.db.elephantsql.com',
+            'PORT': '5432',
+        }
     }
-}
 
 
 # Password validation
