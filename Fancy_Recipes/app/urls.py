2e823a2eb16a192b734e40b39e0e0bad2d34edad
Krystian Kosek <krystian.kosek17@gmail.com>
2021-04-25 14:18:57 +0200

    Added missing comma

--- Fancy_Recipes/app/urls.py
+++ Fancy_Recipes/app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('profile/updateProfile/', views.updateProfile, name='updateProfile'),
-    path('profile/deleteProfile/', views.deleteProfile, name='deleteProfile')
+    path('profile/deleteProfile/', views.deleteProfile, name='deleteProfile'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name = "login"),
 
 
