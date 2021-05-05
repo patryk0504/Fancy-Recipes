1700bf3063ebe686545a38631d2e44d517f8b2a7
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2021-05-05 14:35:09 +0200

    add Ingredient model and basic fill DB

--- Fancy_Recipes/app/models.py
+++ Fancy_Recipes/app/models.py
@@ -12,9 +12,20 @@ class Account(models.Model):
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length = 50, blank = False)
-    price = models.DecimalField(max_digits = 5, decimal_places = 2)
+    name = models.CharField(max_length = 200, blank = False)
 
+    carbohydrate = models.FloatField(blank=True,default=0) 
+    carbohydrate_units = models.CharField(max_length = 10, blank = True,default="")
+
+    energy = models.FloatField(blank=True,default=0) 
+
+    protein = models.FloatField(blank=True,default=0)
+    protein_units = models.CharField(max_length = 10, blank = True,default="")
+
+    fat = models.FloatField(blank=True,default=0)
+    fat_units = models.CharField(max_length = 10, blank = True,default="")
+
+    price = models.DecimalField(max_digits = 5, decimal_places = 2)
 
 class Recipe(models.Model):
     name = models.CharField(max_length = 50, blank = False)
